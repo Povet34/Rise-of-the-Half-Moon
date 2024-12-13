614f2a32a08bba73ead10aa0bad4a6517e0af408
Povet34 <70003696+Povet34@users.noreply.github.com>
2024-12-13 17:47:29 +0900

    playAI시, 글로벌 볼륨을 조작한 뒤 게임 시작

--- Rise-of-the-Half-Moon/Assets/Scripts/Lobby/Lobby.cs
+++ Rise-of-the-Half-Moon/Assets/Scripts/Lobby/Lobby.cs
@@ -1,6 +1,6 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Lobby : MonoBehaviour
@@ -10,11 +10,36 @@ public class Lobby : MonoBehaviour
     [SerializeField] Button SettingsButton;
 
     [SerializeField] Settings settings;
+    [SerializeField] GlobalVolumeController globalVolumeController;
 
     private void Awake()
     {
-        matchmarkingButton.onClick.AddListener(null);
-        playWithAIButton.onClick.AddListener(null);
+        matchmarkingButton.onClick.AddListener(StartMatchMaring);
+        playWithAIButton.onClick.AddListener(PlayWithAI);
         SettingsButton.onClick.AddListener(() => settings.gameObject.SetActive(true));
+    }
+
+    private void PlayWithAI()
+    {
+        if (globalVolumeController != null)
+        {
+            globalVolumeController.SetLensDistortion(-1.0f, 0.01f, 1.0f);
+            StartCoroutine(LoadGameSceneAfterDelay(2.0f));
+        }
+        else
+        {
+            Debug.LogError("GlobalVolumeController is not assigned.");
+        }
+    }
+
+    private IEnumerator LoadGameSceneAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(Definitions.INGAME_SCENE);
+    }
+
+    private void StartMatchMaring()
+    {
+
     }
 }
